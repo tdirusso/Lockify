3e363049a2a3a76b04ea263274a9b25f39321f8f
Timothy DiRusso <tim.dirusso@logmein.com>
2019-04-10 00:28:10 -0700

    Get tracks on redirect (first 20, need to add pagination logic)

--- server/server.js
+++ server/server.js
@@ -1,29 +1,15 @@
 const express = require('express');
+const qs = require('querystring');
 const https = require('https');
 const path = require('path');
 const app = express();
 const port = 8000;
 
+require('dotenv').config();
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
-
-app.get('/', function (req, res) {
-    res.json({ test: 'test' });
-});
-
-
-
-
-const options = {
-    hostname: 'jsonplaceholder.typicode.com',
-    path: '/todos/1',
-    method: 'GET'
-};
-
-
+app.listen(port);
 
 const request = (options) => {
     return new Promise((resolve, reject) => {
@@ -34,11 +20,60 @@ const request = (options) => {
         });
 
         req.on('error', error => reject(error));
-        if (options.method === 'POST') req.write(payload);
+        if (options.method === 'POST') req.write(options.payload);
         req.end();
     })
 };
 
-request(options).then((data) => {
-    console.log(data);
+/******************** API Endpoints ********************/
+
+/* Retreives and returns a Spotify access token given a valid access code */
+app.get('/token', function (req, res) {
+    const code = req.query.code;
+
+    if (!code) {
+        res.status(400).json({ Error: "No access code was provided." });
+    }
+
+    const options = {
+        hostname: 'accounts.spotify.com',
+        path: '/api/token',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        payload: qs.stringify({
+            grant_type: "authorization_code",
+            code: code,
+            redirect_uri: 'http://localhost:3000/dashboard',
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET
+        })
+    };
+
+    return request(options)
+        .then(data => res.json(data))
+        .catch(error => res.json(error));
+});
+
+/* Retreives and returns Spotify tracks for the current user given a valid access token */
+app.get('/tracks', function (req, res) {
+    const token = req.query.token;
+
+    if (!token) {
+        return res.status(400).json({ Error: "No access token was provided." });
+    }
+
+    const options = {
+        hostname: 'api.spotify.com',
+        path: '/v1/me/tracks',
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+        }
+    };
+
+    return request(options)
+        .then(data => res.json(data))
+        .catch(error => res.json(error))
 });
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -4,23 +4,34 @@ const qs = require('querystring');
 
 class Dashboard extends Component {
 
-    constructor(props) {
-        super(props);
-    }
-
-    login() {
-        fetch('https://accounts.spotify.com/api/token')
+    getTracks(code) {
+        fetch(`/token?code=${code}`)
+            .then(data => data.ok ? data.json() : Error(data.statusText))
+            .then((response) => {
+                if (response.error) {
+                    throw new Error(response.error);
+                } else {
+                    const token = JSON.parse(response).access_token;
+                    return fetch(`/tracks?token=${token}`);
+                }
+            })
+            .then(data => data.ok ? data.json() : Error(data.statusText))
+            .then((response) => {
+                console.log(JSON.parse(response));
+            })
+            .catch(error => console.log(error));
     }
 
     render() {
         const query = qs.parse(window.location.search);
-        if (!query['?code']) {
+        const code = query['?code'];
+        if (!code) {
             this.props.history.push('/');
             return null;
         } else {
             return (
                 <div className="container">
-                    {this.login()}
+                    {this.getTracks(code)}
                     Dashboard
                 </div>
             );
