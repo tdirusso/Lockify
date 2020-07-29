e32f18ee07a1ce4218df1ed5bdc7e8a34b79e58e
Timothy DiRusso <timgdirusso@gmail.com>
2020-07-29 15:57:23 -0700

    Pushing heroku build files

---
+++ build/precache-manifest.a941b173d97bbde3bd47ae55f0a9d9e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc04e00d5d074018a7ad",
+    "url": "/static/css/main.265a3754.chunk.css"
+  },
+  {
+    "revision": "fc04e00d5d074018a7ad",
+    "url": "/static/js/main.f4b479cf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d59ff56029bbc1bd6fc5",
+    "url": "/static/js/2.c03652c9.chunk.js"
+  },
+  {
+    "revision": "3b3e58e6dbaffc513fcc4f9fefa17036",
+    "url": "/index.html"
+  }
+];
